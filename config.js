e731e6e5fd91fb8fce4ba97f5049e4db10d6d88b
cehou <ce.hou.gis@gmail.com>
2026-06-13 13:51:45 -0700

    Update Cloudflare tunnel URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Public backend URL (Cloudflare tunnel from the Savio node).
 // Update this whenever the tunnel URL changes, then commit.
-window.WILDFIRE_API = 'https://cellular-infinite-enhancements-void.trycloudflare.com';
+window.WILDFIRE_API = 'https://emerging-pleasant-linked-emails.trycloudflare.com';
