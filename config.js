be9a2c90d450fa19636a09f6e6c99fd45622cbb1
cehou <ce.hou.gis@gmail.com>
2026-06-19 15:21:40 -0700

    update tunnel URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Public backend URL (Cloudflare tunnel from the Savio node).
 // Update this whenever the tunnel URL changes, then commit.
-window.WILDFIRE_API = 'https://slight-exclusive-sub-assets.trycloudflare.com';
+window.WILDFIRE_API = 'https://celebration-wherever-benefits-nato.trycloudflare.com';
